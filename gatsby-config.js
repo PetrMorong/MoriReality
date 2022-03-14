56fc82a3b5360e5aa730feab9eb7b8bd786743a4
Petr Morong <moriandr73@gmail.com>
2022-03-14 08:27:05 +0100

    build fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,5 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
   ],
 };
