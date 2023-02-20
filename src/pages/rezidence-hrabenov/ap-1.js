5dc11f81d132f7d0d05d8c6782c649fbc89c8c5c
Macbook <moriandr73@gmail.com>
2023-02-20 21:04:23 +0100

    upravy hrabenov

--- src/pages/rezidence-hrabenov/ap-1.js
+++ src/pages/rezidence-hrabenov/ap-1.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Layout from "../../components/layout";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import ApSectionOne from "../../components/ApartmentDetail/ApSectionOne";
-import ApSectionTwo from "../../components/ApartmentDetail/ApSectionTwo";
-import ApSectionThree from "../../components/ApartmentDetail/ApSectionThree";
+import React from 'react'
+import Layout from '../../components/layout'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import ApSectionOne from '../../components/ApartmentDetail/ApSectionOne'
+import ApSectionTwo from '../../components/ApartmentDetail/ApSectionTwo'
+import ApSectionThree from '../../components/ApartmentDetail/ApSectionThree'
 
 const data = {
-  sectionOneBg: "v1646637293/hrabenov/apA/1_xiveuc.jpg",
-  layoutImage: "v1646638993/hrabenov/apA/Untitled_9_z1vzyk.png",
-  apName: "Ap. 245/1",
-  projectName: "Rezidence Hrabenov",
-  price: "3 940 000",
-  backLink: "/rezidence-hrabenov",
-  apTitle: "Apartmán Zlatník",
+  sectionOneBg: 'v1646637293/hrabenov/apA/1_xiveuc.jpg',
+  layoutImage: 'v1646638993/hrabenov/apA/Untitled_9_z1vzyk.png',
+  apName: 'Ap. 245/1',
+  projectName: 'Rezidence Hrabenov',
+  price: '3 590 000',
+  backLink: '/rezidence-hrabenov',
+  apTitle: 'Apartmán Zlatník',
   apText: (
     <>
       Vzdušný mezonet se dvěma ložnicemi a nadstandardní obytnou částí. Svým
       prostorovým uspořádáním uspokojí i nároky pro pohodovou rodinnou
-      dovolenou. Náleží k němu vlastní část{" "}
+      dovolenou. Náleží k němu vlastní část{' '}
       <span>zahrady o velikosti 75 m2</span> se samostatným vchodem. V horním
       patře se nachází velký obývací prostor s kuchyní, první ložnice a
       prostorná koupelna. Po točitých schodech se dostanete do spodní části, kde
@@ -27,87 +27,87 @@ const data = {
       <br />
       V ceně prodeje apartmánu je <span>parkovací stání a sklep.</span> Dále
       kompletně zařízená kuchyňská linka včetně elektrospotřebičů, podlahy,
-      obklady, dlažby i sanita. Za příplatek vám také rádi vybavíme apartmán{" "}
+      obklady, dlažby i sanita. Za příplatek vám také rádi vybavíme apartmán{' '}
       <span>kompletně dle návrhu interiérového designéra</span>, od nábytku
       a doplňků až k nádobí či příborům.
     </>
   ),
   sectionOneDetails: [
     {
-      text: "3+",
-      goldText: "kk",
-      desc: "Dispozice",
+      text: '3+',
+      goldText: 'kk',
+      desc: 'Dispozice',
     },
     {
-      text: "63 ",
-      goldText: "m2",
-      desc: "Podlahová Plocha",
+      text: '63 ',
+      goldText: 'm2',
+      desc: 'Podlahová Plocha',
     },
     {
-      text: "2 ",
-      goldText: "NP",
-      desc: "Umístění",
+      text: '2 ',
+      goldText: 'NP',
+      desc: 'Umístění',
     },
     {
-      text: "Jihozápad",
-      goldText: "",
-      desc: "Orientace",
+      text: 'Jihozápad',
+      goldText: '',
+      desc: 'Orientace',
     },
 
     {
-      text: "",
-      goldText: "Hrabenov",
-      desc: "Lokatita",
+      text: '',
+      goldText: 'Hrabenov',
+      desc: 'Lokatita',
     },
   ],
   gallery: [
-    "v1646637295/hrabenov/apA/8_h9o5zs.jpg",
-    "v1646637295/hrabenov/apA/2_dvckd5.jpg",
-    "v1646637293/hrabenov/apA/1_xiveuc.jpg",
-    "v1646637292/hrabenov/apA/6_lzwnwa.jpg",
-    "v1646637292/hrabenov/apA/6_lzwnwa.jpg",
-    "v1646637291/hrabenov/apA/9_kkrr6u.jpg",
-    "v1646637291/hrabenov/apA/9_kkrr6u.jpg",
-    "v1646637289/hrabenov/apA/10_e90kfz.jpg",
-    "v1646637289/hrabenov/apA/5_xptqfv.jpg",
-    "v1645823344/hrabenov/1_frp1mw.jpg",
-    "v1645823341/hrabenov/4_isndkx.jpg",
-    "v1646062243/hrabenov/9_gt3rxh.jpg",
-    "v1646062245/hrabenov/ISO_cjeydz.jpg",
+    'v1646637295/hrabenov/apA/8_h9o5zs.jpg',
+    'v1646637295/hrabenov/apA/2_dvckd5.jpg',
+    'v1646637293/hrabenov/apA/1_xiveuc.jpg',
+    'v1646637292/hrabenov/apA/6_lzwnwa.jpg',
+    'v1646637292/hrabenov/apA/6_lzwnwa.jpg',
+    'v1646637291/hrabenov/apA/9_kkrr6u.jpg',
+    'v1646637291/hrabenov/apA/9_kkrr6u.jpg',
+    'v1646637289/hrabenov/apA/10_e90kfz.jpg',
+    'v1646637289/hrabenov/apA/5_xptqfv.jpg',
+    'v1645823344/hrabenov/1_frp1mw.jpg',
+    'v1645823341/hrabenov/4_isndkx.jpg',
+    'v1646062243/hrabenov/9_gt3rxh.jpg',
+    'v1646062245/hrabenov/ISO_cjeydz.jpg',
   ],
   moreDetails: [
     {
-      text: "2023",
-      goldText: "",
-      desc: "Datum Dokončení",
+      text: '2023',
+      goldText: '',
+      desc: 'Datum Dokončení',
     },
     {
-      text: "",
-      goldText: "3 ",
-      desc: "apartmány v domě",
+      text: '',
+      goldText: '3 ',
+      desc: 'apartmány v domě',
     },
     {
-      text: "Cihlová",
-      goldText: "",
-      desc: "Stavba",
+      text: 'Cihlová',
+      goldText: '',
+      desc: 'Stavba',
     },
     {
-      text: "",
-      goldText: "Parkovací",
-      desc: "místo v ceně",
+      text: '',
+      goldText: 'Parkovací',
+      desc: 'místo v ceně',
     },
   ],
   table: [
-    { label: "Stav", value: "Dostupný k rezervaci" },
-    { label: "Číslo bytu", value: "245/1 " },
-    { label: "Dispozice", value: "3+kk" },
-    { label: "Podlaží", value: "2" },
-    { label: "Podlahová plocha", value: "63m2" },
-    { label: "Orientace", value: "Jihozápad" },
-    { label: "Zahrada", value: "70m2" },
-    { label: "Dokončení", value: "2022" },
+    { label: 'Stav', value: 'Dostupný k rezervaci' },
+    { label: 'Číslo bytu', value: '245/1 ' },
+    { label: 'Dispozice', value: '3+kk' },
+    { label: 'Podlaží', value: '2' },
+    { label: 'Podlahová plocha', value: '63m2' },
+    { label: 'Orientace', value: 'Jihozápad' },
+    { label: 'Zahrada', value: '70m2' },
+    { label: 'Dokončení', value: '2023' },
   ],
-};
+}
 
 const MalaMoravaAp1Page = () => {
   return (
@@ -118,7 +118,7 @@ const MalaMoravaAp1Page = () => {
       <ApSectionThree data={data} />
       <Footer email="dominik.lokoc@homeportal.cz" phone="+420 602 732 879" />
     </Layout>
-  );
-};
+  )
+}
 
-export default MalaMoravaAp1Page;
+export default MalaMoravaAp1Page
--- src/pages/rezidence-hrabenov/ap-3.js
+++ src/pages/rezidence-hrabenov/ap-3.js
@@ -1,19 +1,19 @@
-import React from "react";
-import Layout from "../../components/layout";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import ApSectionOne from "../../components/ApartmentDetail/ApSectionOne";
-import ApSectionTwo from "../../components/ApartmentDetail/ApSectionTwo";
-import ApSectionThree from "../../components/ApartmentDetail/ApSectionThree";
+import React from 'react'
+import Layout from '../../components/layout'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import ApSectionOne from '../../components/ApartmentDetail/ApSectionOne'
+import ApSectionTwo from '../../components/ApartmentDetail/ApSectionTwo'
+import ApSectionThree from '../../components/ApartmentDetail/ApSectionThree'
 
 const data = {
-  sectionOneBg: "v1647260373/hrabenov/AP_C/1_d7bkqe.jpg",
-  layoutImage: "v1647260361/hrabenov/AP_C/13_fjxxrl.jpg",
-  apName: "Ap. 245/3",
-  projectName: "Rezidence Hrabenov",
-  price: "4 140 000",
-  backLink: "/rezidence-hrabenov",
-  apTitle: "Apartmán Blýskač",
+  sectionOneBg: 'v1647260373/hrabenov/AP_C/1_d7bkqe.jpg',
+  layoutImage: 'v1647260361/hrabenov/AP_C/13_fjxxrl.jpg',
+  apName: 'Ap. 245/3',
+  projectName: 'Rezidence Hrabenov',
+  price: '3 990 000',
+  backLink: '/rezidence-hrabenov',
+  apTitle: 'Apartmán Blýskač',
   apText: (
     <>
       Nadčasový podkrovní apartmán s velice útulnou atmosférou pro náročné.
@@ -26,86 +26,86 @@ const data = {
       <br />
       V ceně prodeje apartmánu je <span>parkovací stání a sklep.</span> Dále
       kompletně zařízená kuchyňská linka včetně elektrospotřebičů, podlahy,
-      obklady, dlažby i sanita. Za příplatek vám také rádi vybavíme apartmán{" "}
+      obklady, dlažby i sanita. Za příplatek vám také rádi vybavíme apartmán{' '}
       <span>kompletně dle návrhu interiérového designéra</span>, od nábytku
       a doplňků až k nádobí či příborům.
     </>
   ),
   sectionOneDetails: [
     {
-      text: "3+",
-      goldText: "kk",
-      desc: "Dispozice",
+      text: '3+',
+      goldText: 'kk',
+      desc: 'Dispozice',
     },
     {
-      text: "109 ",
-      goldText: "m2",
-      desc: "Podlahová Plocha",
+      text: '109 ',
+      goldText: 'm2',
+      desc: 'Podlahová Plocha',
     },
     {
-      text: "3 ",
-      goldText: "NP",
-      desc: "Umístění",
+      text: '3 ',
+      goldText: 'NP',
+      desc: 'Umístění',
     },
     {
-      text: "Východ",
-      goldText: "",
-      desc: "Orientace",
+      text: 'Východ',
+      goldText: '',
+      desc: 'Orientace',
     },
 
     {
-      text: "",
-      goldText: "Hrabenov",
-      desc: "Lokatita",
+      text: '',
+      goldText: 'Hrabenov',
+      desc: 'Lokatita',
     },
   ],
   gallery: [
-    "v1647260371/hrabenov/AP_C/7_he8vro.jpg",
-    "v1647260373/hrabenov/AP_C/8_u9gioy.jpg",
-    "v1647260373/hrabenov/AP_C/1_d7bkqe.jpg",
-    "v1647260370/hrabenov/AP_C/2_vxaaxp.jpg",
-    "v1647260362/hrabenov/AP_C/4_yggi8f.jpg",
-    "v1647260369/hrabenov/AP_C/3_pjtuia.jpg",
-    "v1647260366/hrabenov/AP_C/10_qw8y7o.jpg",
-    "v1647260365/hrabenov/AP_C/9_ucw8w0.jpg",
-    "v1647260366/hrabenov/AP_C/5_jkjtzw.jpg",
-    "v1647260366/hrabenov/AP_C/6_kf01af.jpg",
-    "v1647260364/hrabenov/AP_C/11_fnuda1.jpg",
-    "v1647260375/hrabenov/AP_C/12_z2tmcp.jpg",
+    'v1647260371/hrabenov/AP_C/7_he8vro.jpg',
+    'v1647260373/hrabenov/AP_C/8_u9gioy.jpg',
+    'v1647260373/hrabenov/AP_C/1_d7bkqe.jpg',
+    'v1647260370/hrabenov/AP_C/2_vxaaxp.jpg',
+    'v1647260362/hrabenov/AP_C/4_yggi8f.jpg',
+    'v1647260369/hrabenov/AP_C/3_pjtuia.jpg',
+    'v1647260366/hrabenov/AP_C/10_qw8y7o.jpg',
+    'v1647260365/hrabenov/AP_C/9_ucw8w0.jpg',
+    'v1647260366/hrabenov/AP_C/5_jkjtzw.jpg',
+    'v1647260366/hrabenov/AP_C/6_kf01af.jpg',
+    'v1647260364/hrabenov/AP_C/11_fnuda1.jpg',
+    'v1647260375/hrabenov/AP_C/12_z2tmcp.jpg',
   ],
   moreDetails: [
     {
-      text: "2022",
-      goldText: "",
-      desc: "Datum Dokončení",
+      text: '2023',
+      goldText: '',
+      desc: 'Datum Dokončení',
     },
     {
-      text: "",
-      goldText: "3 ",
-      desc: "apartmány v domě",
+      text: '',
+      goldText: '3 ',
+      desc: 'apartmány v domě',
     },
     {
-      text: "Cihlová",
-      goldText: "",
-      desc: "Stavba",
+      text: 'Cihlová',
+      goldText: '',
+      desc: 'Stavba',
     },
     {
-      text: "",
-      goldText: "Parkovací",
-      desc: "místo v ceně",
+      text: '',
+      goldText: 'Parkovací',
+      desc: 'místo v ceně',
     },
   ],
   table: [
-    { label: "Stav", value: "Dostupný k rezervaci" },
-    { label: "Číslo bytu", value: "245/3" },
-    { label: "Dispozice", value: "3+kk" },
-    { label: "Podlaží", value: "3" },
-    { label: "Podlahová plocha", value: "109m2" },
-    { label: "Orientace", value: "Východ" },
-    { label: "Zahrada", value: "109m2" },
-    { label: "Dokončení", value: "2022" },
+    { label: 'Stav', value: 'Dostupný k rezervaci' },
+    { label: 'Číslo bytu', value: '245/3' },
+    { label: 'Dispozice', value: '3+kk' },
+    { label: 'Podlaží', value: '3' },
+    { label: 'Podlahová plocha', value: '109m2' },
+    { label: 'Orientace', value: 'Východ' },
+    { label: 'Zahrada', value: '75m2' },
+    { label: 'Dokončení', value: '2023' },
   ],
-};
+}
 
 const MalaMoravaAp3Page = () => {
   return (
@@ -116,7 +116,7 @@ const MalaMoravaAp3Page = () => {
       <ApSectionThree data={data} />
       <Footer email="dominik.lokoc@homeportal.cz" phone="+420 602 732 879" />
     </Layout>
-  );
-};
+  )
+}
 
-export default MalaMoravaAp3Page;
+export default MalaMoravaAp3Page
