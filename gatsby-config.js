435add11e2fe20936e41fa54eb97d17262e474de
Macbook <moriandr73@gmail.com>
2024-01-24 14:16:55 +0100

    feat: analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,31 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-styled-components",
+ 
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-YSN8FJKHGX",
-      },
-    },
-
-    {
-      resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: '709775591288566',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-YSN8FJKHGX', // Google Analytics / GA
+          // optional
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
     "gatsby-plugin-image",
